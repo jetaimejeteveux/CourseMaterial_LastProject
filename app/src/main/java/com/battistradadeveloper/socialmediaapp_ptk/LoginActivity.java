7158cc1105c7ff0756b6bda980b134fb940d217e
Firman Aulia Insani <akufirman22@gmail.com>
2020-06-10 00:28:24 +0700

    First Commit - Login dan Register

---
+++ app/src/main/java/com/battistradadeveloper/socialmediaapp_ptk/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.battistradadeveloper.socialmediaapp_ptk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
